71e5105b019f65c2355d976a38e203fced94dba6
s6laylo <160312216+s6laylo@users.noreply.github.com>
2026-08-27 15:12:32 +0100

    Completed exercise 1, creating the songs table

---
+++ Learn-SQL-master/my-solutions/1.sql
@@ -0,0 +1,10 @@
+USE record_company;
+
+CREATE TABLE songs (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    length FLOAT NOT NULL,
+    album_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (album_id) REFERENCES albums(id)
+);
